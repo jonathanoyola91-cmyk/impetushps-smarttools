9987245a7de5b8daf799f11cfa4d362144c2aeb7
impetus <jonathan.oyola91@gmail.com>
2026-05-08 11:17:43 -0500

    Fix database config for Render

--- config/settings.py
+++ config/settings.py
@@ -90,9 +90,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=not DEBUG,
+        default=os.environ.get("DATABASE_URL")        
     )
 }
 
